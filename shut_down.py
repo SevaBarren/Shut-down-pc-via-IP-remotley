224ec9cb3f201a86f40e824dcb9eaf4a262057a0
E8-32 <120282062+SevaBarren@users.noreply.github.com>
2024-09-03 15:52:59 -0500

    Update shut_down.py

--- shut_down.py
+++ shut_down.py
@@ -1,17 +1,14 @@
 import argparse
 import os
 
-# Set up the command-line arguments
 parser = argparse.ArgumentParser(description='Shut down multiple computers via IPv4')
 parser.add_argument('-c', '--computers', required=True, nargs='+', help='List of hostnames or IP addresses of the computers to shut down')
 parser.add_argument('-t', '--timeout', type=int, default=30, help='Number of seconds to wait before shutting down the computers')
 parser.add_argument('-f', '--force', action='store_true', help='Force all applications to close when shutting down the computers')
 parser.add_argument('-r', '--reboot', action='store_true', help='Reboot the computers instead of shutting them down')
 
-# Parse the command-line arguments
 args = parser.parse_args()
 
-# Shut down each computer
 for computer in args.computers:
     command = 'shutdown /s' if not args.reboot else 'shutdown /r'
     if args.force:
